77c10f5c10789b80d92b30e027aad713a032649e
oskarpukk <130538957+oskarpukk@users.noreply.github.com>
2024-12-18 23:06:44 +0200

    N

--- chat-app/wsgi.py
+++ chat-app/wsgi.py
@@ -1,7 +1,8 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 from src.servertest.static.js.server import app, socketio
+import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
