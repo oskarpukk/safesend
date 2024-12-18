693cda3d7db4877d63432d3bec591e2401196788
oskarpukk <130538957+oskarpukk@users.noreply.github.com>
2024-12-18 22:58:55 +0200

    n

--- chat-app/wsgi.py
+++ chat-app/wsgi.py
@@ -1,6 +1,10 @@
 from src.servertest.static.js.server import app, socketio
-
-app = socketio.run(app)
+import os
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, 
+                host='0.0.0.0',
+                port=port,
+                debug=False,
+                allow_unsafe_werkzeug=True)
