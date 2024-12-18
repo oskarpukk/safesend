2b187c025abc7e9a1fc18f067a9da73476e2b787
oskarpukk <130538957+oskarpukk@users.noreply.github.com>
2024-12-18 23:00:22 +0200

    n

--- chat-app/wsgi.py
+++ chat-app/wsgi.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     socketio.run(app, 
                 host='0.0.0.0',
                 port=port,
-                debug=False,
-                allow_unsafe_werkzeug=True)
+                debug=False)
